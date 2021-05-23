eb9ed79b436dde904454dd3833c0aae20b9e8ef4
Souvik <souvikde.ns@gmail.com>
2021-05-23 18:08:15 +0530

    trying to fix the IP problem 😢

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,10 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { supabase, Visits } from '../../lib/supabase';
+import { getClientIp } from '@supercharge/request-ip';
 import _ from 'lodash';
 import { label } from '../../lib/svg';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    let IP = req.connection.remoteAddress;
+    let IP = getClientIp(req);
     let { data, error } = await supabase.from("visitors").select('*')
     if (!_.find(data, d => d.ip === IP)) {
         await supabase.from("visitors").insert([{ ip: IP }]);
