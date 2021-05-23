3d712e925867b2a524f120912df73848bb9f1172
Souvik <souvikde.ns@gmail.com>
2021-05-23 16:31:43 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    env: {
-        supabaseUrl: process.env.SUPABASE_URL,
-        anonPublicKey: process.env.ANON_PUBLIC_KEY,
-        githubUserName: 'Souvikns'
-    }
-}
